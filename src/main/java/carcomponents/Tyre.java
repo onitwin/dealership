5fc0efccce41a49e5edd60eece32858d2cf25f10
onitwin <onitwin@gmail.com>
2021-02-09 12:33:56 +0000

    added tyres

--- src/main/java/carcomponents/Tyre.java
+++ src/main/java/carcomponents/Tyre.java
@@ -1,4 +1,28 @@
 package carcomponents;
 
 public class Tyre {
+    private String brand;
+    private int cost;
+
+
+    public Tyre(String brand,int cost){
+        this.brand=brand;
+        this.cost=cost;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
 }
--- src/test/java/TyreTest.java
+++ src/test/java/TyreTest.java
@@ -1,2 +1,24 @@
+import carcomponents.Tyre;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TyreTest {
+    Tyre tyre;
+
+    @Before
+    public void before(){
+        tyre=new Tyre("Goodyear",20);
+    }
+
+    @Test
+    public void canGetBrand(){
+        assertEquals("Goodyear",tyre.getBrand());
+    }
+
+    @Test
+    public void canGetCost(){
+        assertEquals(20,tyre.getCost());
+    }
 }
